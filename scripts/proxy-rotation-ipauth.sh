30665631bb9a33408c1b7f9599dce49bbb180974
Ishaan <ishaan.rocketminecraft@gmail.com>
2020-04-07 11:08:27 -0700

    fixed mistake

--- scripts/proxy-rotation-ipauth.sh
+++ scripts/proxy-rotation-ipauth.sh
@@ -4,6 +4,7 @@ random() {
 	echo
 }
 
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
