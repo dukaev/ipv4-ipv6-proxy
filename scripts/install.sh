73d1227fa35466ee91e6b119db744da003395d4e
Aslan Dukaev <dukaev999@gmail.com>
2020-05-27 12:33:49 +0300

    Revert "Update install.sh"

--- scripts/install.sh
+++ scripts/install.sh
@@ -88,8 +88,8 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s ifconfig.me)
-IP6=$(curl -6 -s ifconfig.me | cut -f1-4 -d':')
+IP4=$(curl -4 -s ifconfig.co)
+IP6=$(curl -6 -s ifconfig.co | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
