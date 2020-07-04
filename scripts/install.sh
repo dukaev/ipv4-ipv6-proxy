ad1bee5f64a0433b46877c5e546fb0706fd0dc1d
Ishaan <ishaan.rocketminecraft@gmail.com>
2020-07-04 12:39:42 -0700

    Fix IPv4/IPv6 local detection

--- scripts/install.sh
+++ scripts/install.sh
@@ -90,8 +90,8 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s ifconfig.co)
-IP6=$(curl -6 -s ifconfig.co | cut -f1-4 -d':')
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
