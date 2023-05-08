065a69ecad948c1aa9f34054fae693a80f067a84
tuanphampmt <43573296+tuanphampmt@users.noreply.github.com>
2023-05-08 16:17:43 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,7 @@ users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"socks -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
